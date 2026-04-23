f125c1e3416a9a65bbf62f34f5314364c2e14f92
Ruben Rivera <riveraagostoruben@gmail.com>
2026-04-23 15:50:58 -0500

    added private member from class

--- Movie.h
+++ Movie.h
@@ -1,1 +1,34 @@
 #include <iostream>
+
+
+class Movie
+
+{
+private:
+    char Title;
+    char Director;
+    char Publish_Date;
+    char Synopsis;
+    Movie* next;
+    Movie* prev;
+
+
+
+public:
+    
+(/* args */);
+    ~
+    ();
+};
+
+
+::
+(/* args */)
+{
+}
+
+
+::~
+()
+{
+}
