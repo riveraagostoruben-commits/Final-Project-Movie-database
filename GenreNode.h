ce1ac6a2546487259b757763119ea5a0f5c7dbcf
Ruben Rivera <riveraagostoruben@gmail.com>
2026-04-24 23:15:50 -0500

    started Genre node added private members and public setters and gettters

---
+++ GenreNode.h
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include "MovieSLL.h"
+using namespace std;
+
+class GenreNode 
+{
+    private:
+    string genre;
+    
+    GenreNode *prev;
+    GenreNode *next;
+
+    MovieSLL *MovList;
+
+
+
+
+
+
+    public:
+    void setGenre(string g1){
+        genre = g1;
+    }
+
+    string getGenre(){
+        return genre;
+    }
+
+    void setNext(GenreNode *gnn){
+        next = gnn;
+    }
+    GenreNode *getNext(){
+        return next;
+    }
+
+    void setPrev(GenreNode * gnp){
+        prev = gnp;
+    }
+
+    GenreNode *getPrev(){
+        return prev;
+    }
+
+
+};
