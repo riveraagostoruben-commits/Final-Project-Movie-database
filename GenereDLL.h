a1e5efea061ecf82464c66a163c8a60c77080d6c
Ruben Rivera <riveraagostoruben@gmail.com>
2026-04-29 20:49:43 -0500

    finishes delete and add from head and tail, finished printlist, and starting sortInsert y alphabetical order

--- GenereDLL.h
+++ GenereDLL.h
@@ -10,6 +10,89 @@ class GenreDLL{
 
     public:
 
+    void addtoHead(string g1,MovieSLL *ml1){
+        if(head!= nullptr){
+            head->setPrev(new GenreNode(g1, ml1,nullptr, head));
+            head = head->getPrev();
+        }
+        else {
+            head = tail = new GenreNode(g1, ml1,nullptr, nullptr);
+        }
+
+    }
+
+    void addtoTail(string g1, MovieSLL *ml1){
+        if(tail!= nullptr){
+            tail->setNext(new GenreNode(g1, ml1, tail, nullptr));
+            tail = tail->getNext();
+
+        }
+        else{
+            tail = head = new GenreNode(g1,ml1, nullptr, nullptr);
+        }
+
+    }
+
+    void deletefromHead(){
+        if(head != nullptr){
+            GenreNode *ptr = head;
+            head = head->getNext();
+            if(head == nullptr){
+                tail = nullptr;
+            }
+            else{
+                head->setPrev(nullptr);
+            }
+            delete ptr;
+        }
+
+    }
+
+    void deltefromTail(){
+        if(head != nullptr){
+            GenreNode *ptr = tail;
+            if(head!=tail){
+                tail =  ptr->getPrev();
+                tail->setNext(nullptr);
+
+            }
+            else{
+                head = tail = nullptr;
+            }
+            delete ptr;
+
+        }
+
+    }
+
+    void sortInsert(string g1, MovieSLL *ml1){
+        if(head == nullptr){
+            head = tail = new GenreNode(g1,m1,nullptr,nullptr);
+        }
+        else{
+            
+        }
+
+
+    }
+
+    void printList(){
+        if(head != nullptr){
+            GenreNode *ptr = head;
+            while(ptr!= nullptr){
+            cout<< ptr->getGenre() << endl;
+            ptr = ptr->getNext();
+            }
+
+        }
+        return;
+
+    }
+
+
+
+
+
 
 };
 
