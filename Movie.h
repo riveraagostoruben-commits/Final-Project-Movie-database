e394e45429a9b2b3e61600f6e8d83f5fc02db732
Ruben Rivera <riveraagostoruben@gmail.com>
2026-05-01 11:13:50 -0500

    Finished adding and making BSTNode ann BSTree calsss

--- Movie.h
+++ Movie.h
@@ -10,11 +10,12 @@ private:
     string Director;
     string Publish_Date;
     string Synopsis;
-    Movie* next;
+    Movie *next;
+    BSTree *cast_crew;
   
 
 public:
-Movie(string t1, string d1, string dt1, string sy1, Movie *nxt = nullptr): Title(t1),Director(d1),Publish_Date(dt1),Synopsis(sy1),next(nxt){}
+Movie(string t1, string d1, string dt1, string sy1, Movie *nxt = nullptr, BSTree *c_c= nullptr): Title(t1),Director(d1),Publish_Date(dt1),Synopsis(sy1),next(nxt), cast_crew(c_c){}
 void setTitle(string t1){
         Title = t1;
     }
@@ -55,6 +56,90 @@ Movie *getNext(){
 }
 
 
+
+
+};
+
+class BSTNode {
+    
+    private:
+    BSTNode *left;
+    BSTNode * right;
+    string name;
+
+    public:
+    BSTNode(BSTNode *l= nullptr, string nam, BSTNode*r = nullptr):left(l),name(nam), right(r){}
+
+    void setName(string n){
+        name = n;
+    }
+    string getName(){
+        return name;
+    }
+    void setLeft(BSTNode * l){
+        left = l;
+    }
+    BSTNode *getLeft(){
+        return left;
+    }
+    void setRight(BSTNode *r){
+        right = r;
+    }
+    BSTNode *getRight(){
+        return right;
+    }
+
+};
+
+class BSTree{
+    private:
+    BSTNode *root;
+    void visitInOrder(BSTNode *node){
+        if(node!=nullptr){
+            visitInOrder(node->getLeft());
+            cout<< "-"<<node->getName()<< "-";
+            visitInOrder(node->getRight());
+        }
+    }
+    void destroyTree(BSTNode* node) {
+    if (node != nullptr) {
+        destroyTree(node->getLeft());  // Delete everything on the left
+        destroyTree(node->getRight()); // Delete everything on the right
+        delete node;                   // Finally, delete the current node
+    }
+}
+    public:
+    BSTree(BSTNode *r =nullptr):root(r){}
+    ~BSTree(){
+        destroyTree(root); 
+        root = nullptr;
+    }
+
+    void Insert(string name){
+        if(root ==nullptr) // if tree empty
+        root = new BSTNode(nullptr,name,nullptr); // insert new root
+
+        else{
+            BSTNode * ptr = root; // pointer to the root
+            BSTNode *sRoot; //trail pointer to not lose connection
+            while(ptr != nullptr){
+                sRoot = ptr;
+                if(name < sRoot->getName()) // if name is less than the root go left
+                    ptr = ptr->getLeft();
+                else
+                ptr = ptr->getRight(); //if name is greater than the root go right
+            }
+            if(name < sRoot->getName()) //insert at the left of sRoot
+            sRoot->setLeft(new BSTNode(nullptr,name,nullptr));
+            else
+            sRoot->setRight(new BSTNode(nullptr,name,nullptr)); // insert at the right
+        }
+    
+    }
+    void PrintInOrder(){
+        visitInOrder(root);
+    }
+   
 
 
 };
